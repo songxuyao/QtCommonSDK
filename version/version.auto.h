ac89383ce97beb91ce1b586c7d6beefc503a7e75
songxuyao <69346590+songxuyao@users.noreply.github.com>
2021-09-23 19:59:52 +0800

    Update version.auto.h

--- version/version.auto.h
+++ version/version.auto.h
@@ -1,5 +1,5 @@
 #pragma once
-#define APP_VERSION_MAJOR 3
+#define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 0
 #define APP_VERSION_PATCH 0
-#define APP_VERSION_BUILD 18
+#define APP_VERSION_BUILD 1
